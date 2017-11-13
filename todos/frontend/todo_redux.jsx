78bcd9e5778cbe51c003a7b413ab1d706f5d6821
Brandt <brandtsheets@gmail.com>
2017-11-13 13:27:49 -0800

    Implement todo store

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', function() {
+  const store = configureStore();
+  window.store = store; //Remove later!
   ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
 });
